5f3c39afd01d5067d1a4e1b5c13c42d112fad18d
jackoliverwerner <jack.oliver.werner@gmail.com>
2016-12-23 00:39:19 -0600

    Fix bugs

--- scrapeGamedayDriver.py
+++ scrapeGamedayDriver.py
@@ -2,11 +2,12 @@ from scrapeGameday import *
 
 # Make URLs
 url_dir = '/Users/jackwerner/Documents/My Stuff/Baseball/Scraping Files/Game IDs'
-writeURLs(2014, url_dir)
+writeURLs(2016, url_dir)
 
 # Save XML files
-xml_dir = '/Users/jackwerner/Documents/My Stuff/Baseball/Scraping Files/Game IDs/2014xml'
-scrapeURLs(url_dir, xml_dir, verbose = True)
+xml_dir = '/Users/jackwerner/Documents/My Stuff/Baseball/Scraping Files/Game IDs/2016xml'
+url_file = url_dir + '/urls2016.txt'
+scrapeURLs(url_file, xml_dir, verbose = True)
 
 # Make into CSV
-directoryToCSV(xml_dir, "pitch_data_2014.csv")
+directoryToCSV(xml_dir, "pitch_data_2016.csv")
